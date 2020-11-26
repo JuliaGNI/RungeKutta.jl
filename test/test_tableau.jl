cbc9fb22b5d31df492741d36279f4ce50de80662
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-11-26 09:48:25 +0100

    Add comprehensive tests for tableau.

--- test/test_tableau.jl
+++ test/test_tableau.jl
@@ -1,8 +1,42 @@
 @testset "$(rpad("Tableau",80))" begin
 
-    s = 2
-    g = TableauGauss(s)
+    using RungeKutta: name, order, nstages, coefficients, weights, nodes
 
-    @test check_symmetry(g) == Array{Bool}(ones(s,s))
+    for s in 1:5
+        for T ∈ (Float64, BigFloat)
+
+            a = rand(T,s,s)
+            b = rand(T,s)
+            c = rand(T,s)
+
+            tab1 = Tableau(:Test, 2s, a, b, c)
+            tab2 = Tableau(:Test, 2s, s, a, b, c)
+            tab3 = Tableau{T}(:Test, 2s, a, b, c)
+            tab4 = Tableau{T}(:Test, 2s, s, a, b, c)
+            tabϵ = Tableau(:Test, 2s, a .+ 4eps(T) .* rand(T,s,s),
+                                      b .+ 4eps(T) .* rand(T,s),
+                                      c .+ 4eps(T) .* rand(T,s))
+
+            @test tab1 ≈ tab2 ≈ tab3 ≈ tab4
+            @test tab1 == tab2 == tab3 == tab4
+            @test tab1 === tab2 === tab3 === tab4
+            @test hash(tab1) == hash(tab2) == hash(tab3) == hash(tab4)
+
+            @test tab1 ≈ tabϵ
+            @test tab1 != tabϵ
+            @test tab1 !== tabϵ
+            @test hash(tab1) != hash(tabϵ)
+
+            @test eltype(tab1) == eltype(tab2) == eltype(tab3) == eltype(tab4) == T
+
+            @test name(tab1) == :Test
+            @test order(tab1) == 2s
+            @test nstages(tab1) == s
+            @test coefficients(tab1) == a
+            @test weights(tab1) == b
+            @test nodes(tab1) == c
+
+        end
+    end
 
 end
