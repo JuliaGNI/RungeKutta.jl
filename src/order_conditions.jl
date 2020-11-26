00a2b094e0f9f9bc43c625203ab60e5c0f6333a1
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-11-26 11:48:24 +0100

    Clean up order_conditions_*().

--- src/order_conditions.jl
+++ src/order_conditions.jl
@@ -1,41 +1,41 @@
 
-function check_order_conditions_b(coeff::Tableau{T}, k) where {T}
+function check_order_conditions_b(tab::Tableau{T}, k) where {T}
     local res::T = 0
 
-    for i in 1:coeff.s
-        res += coeff.b[i] * coeff.c[i]^(k-1)
+    for i in 1:tab.s
+        res += tab.b[i] * tab.c[i]^(k-1)
     end
 
     return isapprox(res, 1/k, atol=16*eps(T), rtol=16*eps(T))
 end
 
 
-function check_order_conditions_c(coeff::Tableau{T}, k) where {T}
-    local order  = falses(coeff.s)
+function check_order_conditions_c(tab::Tableau{T}, k) where {T}
+    local order  = falses(tab.s)
     local res::T
 
-    for i in 1:size(coeff.a, 1)
+    for i in axes(tab.a, 1)
         res = 0
-        for j in 1:size(coeff.a, 2)
-            res += coeff.a[i,j] * coeff.c[j]^(k-1)
+        for j in axes(tab.a, 2)
+            res += tab.a[i,j] * tab.c[j]^(k-1)
         end
-        order[i] = isapprox(res, coeff.c[i]^k/k, atol=16*eps(T), rtol=16*eps(T))
+        order[i] = isapprox(res, tab.c[i]^k/k, atol=16*eps(T), rtol=16*eps(T))
     end
 
     order
 end
 
 
-function check_order_conditions_d(coeff::Tableau{T}, k) where {T}
-    local order  = falses(coeff.s)
+function check_order_conditions_d(tab::Tableau{T}, k) where {T}
+    local order  = falses(tab.s)
     local res::T
 
-    for i in 1:size(coeff.a, 1)
+    for i in axes(tab.a, 1)
         res = 0
-        for j in 1:size(coeff.a, 2)
-            res += coeff.b[j] * coeff.c[j]^(k-1) * coeff.a[j,i]
+        for j in axes(tab.a, 2)
+            res += tab.b[j] * tab.c[j]^(k-1) * tab.a[j,i]
         end
-        order[i] = isapprox(res, coeff.b[i] * (1 - coeff.c[i]^k)/k, atol=16*eps(T), rtol=16*eps(T))
+        order[i] = isapprox(res, tab.b[i] * (1 - tab.c[i]^k)/k, atol=16*eps(T), rtol=16*eps(T))
     end
 
     order
