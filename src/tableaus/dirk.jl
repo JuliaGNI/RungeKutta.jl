577e6ba410737ca8b993caae5c67cc9cd646a1ed
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-12-07 17:27:57 +0100

    Cleanup DIRK coefficients.

--- src/tableaus/dirk.jl
+++ src/tableaus/dirk.jl
@@ -1,10 +1,10 @@
 
 "Tableau of Crank-Nicolson two-stage, 2nd order method"
-    a = @big [[ 0      0    ]
-              [ 1//2   1//2 ]]
-    b = @big  [ 1//2,  1//2 ]
-    c = @big  [ 0,     1    ]
 function TableauCrankNicolson(::Type{T}) where {T}
+    a = @big [[ 0     0   ]
+              [ 1/2   1/2 ]]
+    b = @big  [ 1/2,  1/2 ]
+    c = @big  [ 0,    1   ]
     o = 2
 
     Tableau{T}(:cranknicolson, o, a, b, c)
@@ -13,11 +13,11 @@ end
 TableauCrankNicolson() = TableauCrankNicolson(Float64)
 
 "Tableau of Kraaijevanger and Spijker's two-stage, 2nd order method"
-    a = @big [[ 1//2   0    ]
-              [-1//2   2    ]]
-    b = @big  [-1//2,  3//2 ]
-    c = @big  [ 1//2,  3//2 ]
 function TableauKraaijevangerSpijker(::Type{T}) where {T}
+    a = @big [[ 1/2   0   ]
+              [-1/2   2   ]]
+    b = @big  [-1/2,  3/2 ]
+    c = @big  [ 1/2,  3/2 ]
     o = 2
 
     Tableau{T}(:kraaijevangerspijker, o, a, b, c)
@@ -26,11 +26,11 @@ end
 TableauKraaijevangerSpijker() = TableauKraaijevangerSpijker(Float64)
 
 "Tableau of Qin and Zhang's symplectic two-stage, 2nd order method"
-    a = @big [[ 1//4   0    ]
-              [ 1//2   1//4 ]]
-    b = @big  [ 1//2,  1//2 ]
-    c = @big  [ 1//4,  3//4 ]
 function TableauQinZhang(::Type{T}) where {T}
+    a = @big [[ 1/4   0   ]
+              [ 1/2   1/4 ]]
+    b = @big  [ 1/2,  1/2 ]
+    c = @big  [ 1/4,  3/4 ]
     o = 2
 
     Tableau{T}(:qinzhang, o, a, b, c)
@@ -39,12 +39,12 @@ end
 TableauQinZhang() = TableauQinZhang(Float64)
 
 "Tableau of Crouzeix's two-stage, 3rd order method"
-    fac = @big 0.5/√3
-    a = @big [[ 0.5+fac  0       ]
-              [  -2*fac  0.5+fac ]]
-    b = @big  [ 0.5,     0.5     ]
-    c = @big  [ 0.5+fac, 0.5-fac ]
 function TableauCrouzeix(::Type{T}) where {T}
+    fac = @big 1/2 / √3
+    a = @big [[ 1/2+fac  0       ]
+              [  -2*fac  1/2+fac ]]
+    b = @big  [ 1/2,     1/2     ]
+    c = @big  [ 1/2+fac, 1/2-fac ]
     o = 3
 
     Tableau{T}(:crouzeix, o, a, b, c)
