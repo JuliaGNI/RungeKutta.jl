4bfb02f8d903a5f16adf790c9b672b570dc99a8c
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-01-15 11:52:32 +0100

    Fix docstrings of Radau methods.

--- src/tableaus/radau.jl
+++ src/tableaus/radau.jl
@@ -135,7 +135,7 @@ Reference:
     Journal of Computational Mathematics, Volume 11, Pages 250-260, 1993.
 """
 
-@doc raw"""
+"""
 Radau IB tableau with s stages
 
 ```julia
@@ -144,10 +144,10 @@ TableauRadauIB(s) = TableauRadauIB(Float64, s)
 ```
 The constructor takes the number of stages `s` and optionally the element type `T` of the tableau.
 
-Coefficients are taken as $a^B = \frac{1}{2} ( a^A + \bar{a}^A )$ where $a^A$ are the coefficients
-of the Radau IA method and $\bar{a}^A$ are computed such that the symplecticity conditions
-$b_{i} \bar{a}_{i,j} + \bar{b}_{j} a_{j,i} = b_{i} \bar{b}_{j}$ and $b_{i} = \bar{b}_i$ hold for
-all $1 \le i,j \le s$.
+Coefficients are taken as ``a^B = \\frac{1}{2} ( a^A + \\bar{a}^A )`` where ``a^A`` are the coefficients
+of the Radau IA method and ``\\bar{a}^A`` are computed such that the symplecticity conditions
+``b_{i} \\bar{a}_{i,j} + \\bar{b}_{j} a_{j,i} = b_{i} \\bar{b}_{j}`` and ``b_{i} = \\bar{b}_i`` hold for
+all ``1 \\le i,j \\le s``.
 
 $(reference(Val(:RadauIB)))
 """
@@ -206,7 +206,7 @@ Reference:
     Journal of Computational Mathematics, Volume 11, Pages 250-260, 1993.
 """
 
-@doc raw"""
+"""
 Radau IIB tableau with s stages
 
 ```julia
@@ -215,10 +215,10 @@ TableauRadauIIB(s) = TableauRadauIIB(Float64, s)
 ```
 The constructor takes the number of stages `s` and optionally the element type `T` of the tableau.
 
-Coefficients are taken as $a^B = \frac{1}{2} ( a^A + \bar{a}^A )$ where $a^A$ are the coefficients
-of the Radau IIA method and $\bar{a}^A$ are computed such that the symplecticity conditions
-$b_{i} \bar{a}_{i,j} + \bar{b}_{j} a_{j,i} = b_{i} \bar{b}_{j}$ and $b_{i} = \bar{b}_i$ hold for
-all $1 \le i,j \le s$.
+Coefficients are taken as ``a^B = \\frac{1}{2} ( a^A + \\bar{a}^A )`` where ``a^A`` are the coefficients
+of the Radau IIA method and ``\\bar{a}^AV are computed such that the symplecticity conditions
+``b_{i} \\bar{a}_{i,j} + \\bar{b}_{j} a_{j,i} = b_{i} \\bar{b}_{j}`` and ``b_{i} = \\bar{b}_i`` hold for
+all ``1 \\le i,j \\le s``.
 
 $(reference(Val(:RadauIIB)))
 """
